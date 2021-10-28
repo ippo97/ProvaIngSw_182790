4700bec96f134de27cb1e72d3b2f8e73aa370df4
ippo97 <33878179+ippo97@users.noreply.github.com>
2021-10-28 10:16:59 +0200

    PrimoCommit

---
+++ provaIngSwIppolito/src/main/java/ordinamento/MyListUtil.java
@@ -0,0 +1,42 @@
+package ordinamento;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MyListUtil {
+
+	public static void main(String[] args) {
+		List<Integer> numeri = new ArrayList<Integer>();
+		numeri.add(10);
+		numeri.add(8);
+		numeri.add(12);
+		numeri.add(6);
+
+		List<Integer> s = sort(numeri, 1);
+		System.out.println(s);
+
+	}
+
+	public static List<Integer> sort(List<Integer> list, int order) {
+
+		if(list.isEmpty())
+			return null;
+		
+		if (order == 0) {
+			Collections.sort(list);
+			return list;
+		}
+
+		else if (order == 1) {
+			list.sort(null);
+			Collections.reverse(list);
+			return list;
+		}
+
+		return null;
+
+	}
+
+}
